79fa767659711d5c65b2781a48313aff0bf0eed2
elazar <elazarg@gmail.com>
2017-08-02 18:09:26 +0300

    Add monty hall

--- examples.py
+++ examples.py
@@ -4,16 +4,17 @@ declare("${A.name} and ${B.name} are getting married")
 
 #[Example: puzzle]
 A = join('A')
-q = receive(int) @ A
+q = receive[int] @ A
 # (The above could be performed locally)
 B = join('Solver')
-m, n = receive( (int, int) ) @ B
+mn = receive[(int, int)] @ B
+m, n = mn
 require(m * n == q)
 declare("${B.name} solved the problem!")
 
 #[Example: trusted simultaneous game]
 Even, Odd = join('Even', 'Odd')
-x, y = receive(bool) @ (Even, Odd)
+x, y = receive[bool] @ (Even, Odd)
 if x == y:
     declare("${Even.name} won")
 else:
@@ -21,7 +22,7 @@ else:
 
 #[Example: simultaneous game]
 Even, Odd = join('Even', 'Odd')
-x, y = receive_independent(bool) @ (Even, Odd)
+x, y = receive_independent[bool] @ (Even, Odd)
 if x == y:
     declare("${Even.name} won")
 else:
@@ -29,7 +30,7 @@ else:
 
 #[Example: simultaneous game with payment]
 Even, Odd = join('Even', 'Odd')
-x, y = receive_independent(bool) @ (Even, Odd)
+x, y = receive_independent[bool] @ (Even, Odd)
 if x == y:
     pay(Even, 100)
 else:
@@ -38,7 +39,7 @@ else:
 #[Example: Binary option / external arbitration]
 Oracle = join(0x2346234)
 More, Less = join('More', 'Less')
-is_more = receive(bool) @ Oracle
+is_more = receive[bool] @ Oracle
 if is_more:
     pay(More, 100)
 else:
@@ -59,9 +60,23 @@ declare("${B.name} has won")
 Bidders = join_many('Bidder')
 max = 0
 Bidder = NOBODY
-for bid in receive_independent(int) @ Bidders:   # should be performed lazily, on the reveal
+for bid in receive_independent[int] @ Bidders:   # should be performed lazily, on the reveal
     if bid > max:
         max = bid
         Bidder = dib.id
 
 declare("${B.name} has won")
+
+#[Example: Monty Hall]
+Host, Guest = join('Host', 'Guest')
+with hidden[int]('car') @ Host as car:
+    # `car` value is unusabe inside this block
+    door1 = receive[int] @ Guest
+    goat = receive[int] @ Host;    require(goat != door1)
+    door2 = receive[int] @ Guest;  require(door2 != goat)
+
+require(goat != car)  # should blame host, since he owns both values. This is simple taint tracking, but is inferrable at compile time and can be made explicit
+if door2 == car:
+    declare("${Guest.name} won")
+else:
+    declare("${Host.name} won")
