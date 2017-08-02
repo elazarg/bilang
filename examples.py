24a111acdef67dc6c21e2a0797a6fd348c842737
elazar <elazarg@gmail.com>
2017-08-02 17:05:32 +0300

    add examples

---
+++ examples.py
@@ -0,0 +1,67 @@
+#[Example: joint declaration]
+(A, B) = join('A', 'B')
+declare("${A.name} and ${B.name} are getting married")
+
+#[Example: puzzle]
+A = join('A')
+q = receive(int) @ A
+# (The above could be performed locally)
+B = join('Solver')
+m, n = receive( (int, int) ) @ B
+require(m * n == q)
+declare("${B.name} solved the problem!")
+
+#[Example: trusted simultaneous game]
+Even, Odd = join('Even', 'Odd')
+x, y = receive(bool) @ (Even, Odd)
+if x == y:
+    declare("${Even.name} won")
+else:
+    declare("${Odd.name} won")
+
+#[Example: simultaneous game]
+Even, Odd = join('Even', 'Odd')
+x, y = receive_independent(bool) @ (Even, Odd)
+if x == y:
+    declare("${Even.name} won")
+else:
+    declare("${Odd.name} won")
+
+#[Example: simultaneous game with payment]
+Even, Odd = join('Even', 'Odd')
+x, y = receive_independent(bool) @ (Even, Odd)
+if x == y:
+    pay(Even, 100)
+else:
+    pay(Odd, 100)
+
+#[Example: Binary option / external arbitration]
+Oracle = join(0x2346234)
+More, Less = join('More', 'Less')
+is_more = receive(bool) @ Oracle
+if is_more:
+    pay(More, 100)
+else:
+    pay(Less, 100)
+
+#[Example: Auction without payment and timeouts]
+join Bidders...     # Bidders is an array of length > 0
+max = 0
+Bidder = NOBODY
+for bid in receive(int) @ Bidders: # performed lazily
+    if bid > max:
+        max = bid
+        Bidder = dib.id
+
+declare("${B.name} has won")
+
+#[Example: Blind auction without payment and timeouts]
+join Bidders...     # Bidders is an array of length > 0
+max = 0
+Bidder = NOBODY
+for bid in receive_independent(int) @ Bidders:   # should be performed lazily, on the reveal
+    if bid > max:
+        max = bid
+        Bidder = dib.id
+
+declare("${B.name} has won")
