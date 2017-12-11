e668394ca1f3f2f430ca4392409b89e3117f315b
Elazar Gershuni <elazarg@gmail.com>
2017-12-11 18:02:56 +0200

    Handle JoinSet

--- bsp/scala/src/main/bsp/Model.scala
+++ bsp/scala/src/main/bsp/Model.scala
@@ -1,7 +1,6 @@
 object T {
   type RoleVarname = String
   type Varname = String
-  case class Var(role: RoleVarname, v: Varname)
   type Exp = String
   type WhereExp = String
   type FoldExp = String
@@ -15,21 +14,24 @@ object V {
 import T._
 import V._
 
+case class Var(role: RoleVarname, v: Varname, agent: Agent)
+
 import scala.collection.mutable
 
 sealed abstract class Action
 case class Private() extends Action
 case class Publish(w: WhereExp) extends Action
-case class Join() extends Action
-case class JoinSet() extends Action
+case class Join(single: Boolean) extends Action
 
 case class Fold(exp: FoldExp, v: Varname, init: Value)
 
-case class BigStepProg(
+case class BigStep(
   action: Map[RoleVarname, (Action, Varname, Option[Fold])],
   timeout: Int,
   commands: List[(Varname, Exp)]
 )
+
+case class Program(steps: Seq[BigStep])
 
 object Main {
   private val requests = mutable.HashMap[Var, Value]()
@@ -46,35 +48,37 @@ object Main {
     // initialize fold expression
   }
 
-  def receive(s: BigStepProg, from: Agent, role: RoleVarname, value: Value): Unit = {
+  def receive(s: BigStep, sender: Agent, role: RoleVarname, value: Value): Unit = {
     val (action, varname, maybeFold) = s.action(role)
-    val v = Var(role, varname)
-    val owner = Γ.get(Var(role, "owner"))
+    val v = Var(role, varname, sender)
+    val ownerVar = Var(role, "owner", 0)
+    val owner = Γ.get(ownerVar)
     action match {
-      case Join() =>    require(owner.isEmpty)
-      case JoinSet() =>
-        // join set - indexed by sender id?
-      case Private() => require(owner.contains(from))
+      case Join(bound) =>
+        if (bound) require(owner.isEmpty)
+        Γ += ownerVar -> sender
+      case Private() =>
+        require(owner.contains(sender))
       case Publish(where) =>
-        require(owner.contains(from))
+        require(owner.get == sender)
         require(hash(value) == Γ(v))
         require(evaluate(where, Γ + (v -> value)) != 0)
         for (fold <- maybeFold) {
-            require(!requests.contains(v))
+          require(!requests.contains(v))
           Γ += (global(fold.v) -> evaluate(fold.exp, Γ))
         }
     }
     requests.put(v, value)
   }
 
-  def progress(s: BigStepProg): Unit = {
-    val vars: Set[Var] = (s.action map { case (k, (_, v, _)) => Var(k, v) }).toSet
-    val assigned = requests.keySet
+  def progress(s: BigStep): Unit = {
+    val vars = (s.action map { case (k, (_, v, _)) => (k, v) }).toSet
+    val assigned = requests.keySet.map {case Var(role, v, _) => (role, v)}
     assert(assigned.subsetOf(vars))
     require(s.timeout <= time || vars == assigned)
     Γ ++= requests
     requests.clear()
   }
 
-  private def global(v: Varname) = Var("Global", v)
+  private def global(v: Varname) = Var("Global", v, 0)
 }
