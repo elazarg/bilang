c6ca7ecf53e9c2289d3df5510f205ce95f69bfd8
Elazar Gershuni <elazarg@gmail.com>
2017-12-12 19:24:19 +0200

    restructure: requests are only for keeping record of sent messages

--- bsp/scala/src/main/bsp/Model.scala
+++ bsp/scala/src/main/bsp/Model.scala
@@ -1,113 +1,100 @@
 import scala.collection.mutable
 
-object T {
-  type RoleVarname = String
-  type Varname = String
-  type FoldExp = String
-}
-
-object V {
-  type Value = Int
+object Syntax {
+  type Role = String
+  type Name = String
   type Agent = Int
+
+  sealed abstract class Exp
+  case class Var(role: Role, name: Name) extends Exp
+  case class BinOp(op: (Value, Value) => Value, left: Exp, right: Exp) extends Exp
+  case class UnOp(op: Value => Value, e: Exp) extends Exp
+
+  sealed abstract class Value extends Exp
+  case class Num(n: Int) extends Value
+  case class Bool(t: Boolean) extends Value
+
+  sealed abstract class Action
+  case class Private() extends Action
+  case class Publish(w: Exp) extends Action
+  case class Join(single: Boolean) extends Action
+
+  sealed abstract class Stmt
+  case class Assign(name: Name, e: Exp) extends Stmt
+
+  case class Fold(exp: Exp, v: Name, init: Value)
+
+  case class LocalStep(action: Action, name: Name)
+
+  case class BigStep(action: Map[Role, (LocalStep, Option[Fold])], timeout: Int, commands: Seq[Stmt])
+
+  case class Program(steps: Seq[BigStep])
 }
 
-import T._
-import V._
+import Syntax._
 
-sealed abstract class Exp
-case class Name(s: String) extends Exp
-case class Num(n: Int) extends Exp
-case class BinOp(op: (Num, Num) => Num, left: Exp, right: Exp) extends Exp
+object Model {
+  type Scope = mutable.Map[Var, Value]
+  private val Γ = Map[Agent, Scope]()
+  private val global: Scope = mutable.Map[Var, Value]()
 
-sealed abstract class BoolExp
-case class BName(s: String) extends BoolExp
-case class Bool(t: Boolean) extends BoolExp
-case class Not(e: BoolExp) extends BoolExp
-case class CmpOp(op: (Num, Num) => Bool, left: Exp, right: Exp) extends BoolExp
-case class BoolOp(op: (Bool, Bool) => Bool, left: BoolExp, right: BoolExp) extends BoolExp
-
-
-sealed abstract class Action
-case class Private() extends Action
-case class Publish(w: BoolExp) extends Action
-case class Join(single: Boolean) extends Action
-
-case class Fold(exp: FoldExp, v: Varname, init: Value)
-
-case class Cell(role: RoleVarname, v: Varname, agent: Agent)
-
-case class BigStep(
-  action: Map[RoleVarname, (Action, Varname, Option[Fold])],
-  timeout: Int,
-  commands: List[(Varname, Exp)]
-)
-
-case class Program(steps: Seq[BigStep])
-
-object Main {
-  private val requests = mutable.HashMap[Cell, Value]()
-  private var Γ = Map[Cell, Value]()
+  private val requests = mutable.Map[Agent, Var]()
+  private val owner = mutable.Map[Role, Agent]()
 
   def time = 0
 
   def require(condition: Boolean): Unit = { }
 
-  def eval(e: Exp)(implicit ctx: Map[Name, Num]): Num = {
+  def eval(e: Exp, ctx: Scope): Value = {
     e match {
       case x @ Num(_) => x
-      case v @ Name(_) => ctx(v)
-      case BinOp(op, left, right) => op(eval(left), eval(right))
-    }
-  }
-
-  def eval(e: BoolExp)(implicit ctx: Map[Name, Bool]): Bool = {
-    e match {
       case x @ Bool(_) => x
-      case v @ BName(_) => ctx(v)
-      case CmpOp(op, left, right) => op(eval(left), eval(right))
-      case BoolOp(op, left, right) => op(eval(left), eval(right))
+      case v @ Var(_, _) => ctx(v)
+      case UnOp(op, arg) => op(eval(arg, ctx))
+      case BinOp(op, left, right) => op(eval(left, ctx), eval(right, ctx))
     }
   }
 
-  def evaluate(where: BoolExp, context: Map[Cell, Value]): Value = ???
+  def hash(value: Value): Num = Num(value.hashCode)
 
-  def hash(value: Value): Int = value.hashCode
-
-  def pre_step(): Unit = {
-    // initialize fold expression
+  def pre_step(s: BigStep): Unit = {
+    for ( (k, (step, Some(fold))) <- s.action)
+      global += (Var("Global", step.name) -> eval(fold.init, global))
   }
 
-  def receive_request(s: BigStep, sender: Agent, role: RoleVarname, value: Value): Unit = {
-    val (action, varname, maybeFold) = s.action(role)
-    val v = Cell(role, varname, sender)
-    val ownerCell = Cell(role, "owner", 0)
-    val owner = Γ.get(ownerCell)
+  def receive_request(s: BigStep, sender: Agent, role: Role, value: Value): Unit = {
+    // assume each sender must only send one message
+    val (LocalStep(action, name), maybeFold) = s.action(role)
+    val v = Var(role, name)
+    val local = Γ(sender)
+    require(!requests.get(sender).contains(v))
     action match {
       case Join(single) =>
-        if (single) require(owner.isEmpty)
-        Γ += ownerCell -> sender
+        if (single) require(!owner.contains(role))
+        owner.put(role, sender)
       case Private() =>
-        require(owner.contains(sender))
+        require(owner.get(role).contains(sender))
       case Publish(where) =>
-        require(owner.get == sender)
-        require(hash(value) == Γ(v))
-        require(evaluate(where, Γ + (v -> value)) != 0)
+        require(owner.get(role).contains(sender))
+        require(hash(value) == local(v))
+        require(eval(where, global ++ local + (v -> value)) != Bool(false))
         for (fold <- maybeFold) {
-          require(!requests.contains(v))
-          Γ += (global(fold.v) -> evaluate(fold.exp, Γ))
+          global += (Var("Global", fold.v) -> eval(fold.exp, global ++ local))
         }
     }
-    requests.put(v, value)
+    requests.put(sender, v)
   }
 
   def progress(s: BigStep): Unit = {
-    val vars = (s.action map { case (k, (_, v, _)) => (k, v) }).toSet
-    val assigned = requests.keySet.map {case Cell(role, v, _) => (role, v)}
+    val vars = (s.action map { case (role, (LocalStep(_, v), fold)) => Var(role, v) }).toSet
+    val assigned = requests.values.toSet // TODO: perhaps testing fold result is better
     assert(assigned.subsetOf(vars))
     require(s.timeout <= time || vars == assigned)
-    Γ ++= requests
-    requests.clear()
-  }
 
-  private def global(v: Varname) = Cell("Global", v, 0)
+    requests.clear()
+    for (Assign(name, exp) <- s.commands) {
+      global += Var("Global", name) -> eval(exp, global)
+      // fire event here
+    }
+  }
 }
