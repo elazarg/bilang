8b263bb02d7c68e3fab80bb523610c6af17bc050
Elazar Gershuni <elazarg@gmail.com>
2017-12-12 09:20:03 +0200

    Eval

--- bsp/scala/src/main/bsp/Model.scala
+++ bsp/scala/src/main/bsp/Model.scala
@@ -1,8 +1,8 @@
+import scala.collection.mutable
+
 object T {
   type RoleVarname = String
   type Varname = String
-  type Exp = String
-  type WhereExp = String
   type FoldExp = String
 }
 
@@ -14,16 +14,27 @@ object V {
 import T._
 import V._
 
-case class Var(role: RoleVarname, v: Varname, agent: Agent)
+sealed abstract class Exp
+case class Name(s: String) extends Exp
+case class Num(n: Int) extends Exp
+case class BinOp(op: (Num, Num) => Num, left: Exp, right: Exp) extends Exp
 
-import scala.collection.mutable
+sealed abstract class BoolExp
+case class BName(s: String) extends BoolExp
+case class Bool(t: Boolean) extends BoolExp
+case class Not(e: BoolExp) extends BoolExp
+case class CmpOp(op: (Num, Num) => Bool, left: Exp, right: Exp) extends BoolExp
+case class BoolOp(op: (Bool, Bool) => Bool, left: BoolExp, right: BoolExp) extends BoolExp
+
 
 sealed abstract class Action
 case class Private() extends Action
-case class Publish(w: WhereExp) extends Action
+case class Publish(w: BoolExp) extends Action
 case class Join(single: Boolean) extends Action
 
 case class Fold(exp: FoldExp, v: Varname, init: Value)
+
+case class Cell(role: RoleVarname, v: Varname, agent: Agent)
 
 case class BigStep(
   action: Map[RoleVarname, (Action, Varname, Option[Fold])],
@@ -34,13 +45,31 @@ case class BigStep(
 case class Program(steps: Seq[BigStep])
 
 object Main {
-  private val requests = mutable.HashMap[Var, Value]()
-  private var Γ = Map[Var, Value]()
+  private val requests = mutable.HashMap[Cell, Value]()
+  private var Γ = Map[Cell, Value]()
 
   def time = 0
 
   def require(condition: Boolean): Unit = { }
-  def evaluate(where: WhereExp, context: Map[Var, Value]): Value = ???
+
+  def eval(e: Exp)(implicit ctx: Map[Name, Num]): Num = {
+    e match {
+      case x @ Num(_) => x
+      case v @ Name(_) => ctx(v)
+      case BinOp(op, left, right) => op(eval(left), eval(right))
+    }
+  }
+
+  def eval(e: BoolExp)(implicit ctx: Map[Name, Bool]): Bool = {
+    e match {
+      case x @ Bool(_) => x
+      case v @ BName(_) => ctx(v)
+      case CmpOp(op, left, right) => op(eval(left), eval(right))
+      case BoolOp(op, left, right) => op(eval(left), eval(right))
+    }
+  }
+
+  def evaluate(where: BoolExp, context: Map[Cell, Value]): Value = ???
 
   def hash(value: Value): Int = value.hashCode
 
@@ -48,15 +77,15 @@ object Main {
     // initialize fold expression
   }
 
-  def receive(s: BigStep, sender: Agent, role: RoleVarname, value: Value): Unit = {
+  def receive_request(s: BigStep, sender: Agent, role: RoleVarname, value: Value): Unit = {
     val (action, varname, maybeFold) = s.action(role)
-    val v = Var(role, varname, sender)
-    val ownerVar = Var(role, "owner", 0)
-    val owner = Γ.get(ownerVar)
+    val v = Cell(role, varname, sender)
+    val ownerCell = Cell(role, "owner", 0)
+    val owner = Γ.get(ownerCell)
     action match {
-      case Join(bound) =>
-        if (bound) require(owner.isEmpty)
-        Γ += ownerVar -> sender
+      case Join(single) =>
+        if (single) require(owner.isEmpty)
+        Γ += ownerCell -> sender
       case Private() =>
         require(owner.contains(sender))
       case Publish(where) =>
@@ -73,12 +102,12 @@ object Main {
 
   def progress(s: BigStep): Unit = {
     val vars = (s.action map { case (k, (_, v, _)) => (k, v) }).toSet
-    val assigned = requests.keySet.map {case Var(role, v, _) => (role, v)}
+    val assigned = requests.keySet.map {case Cell(role, v, _) => (role, v)}
     assert(assigned.subsetOf(vars))
     require(s.timeout <= time || vars == assigned)
     Γ ++= requests
     requests.clear()
   }
 
-  private def global(v: Varname) = Var("Global", v, 0)
+  private def global(v: Varname) = Cell("Global", v, 0)
 }
