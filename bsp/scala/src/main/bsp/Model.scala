f396e7792b260064fd531ead56139b17a4e0576e
Elazar Gershuni <elazarg@gmail.com>
2017-12-12 22:33:54 +0200

    Split files and use syntactic ops

--- bsp/scala/src/main/bsp/Model.scala
+++ bsp/scala/src/main/bsp/Model.scala
@@ -1,37 +1,9 @@
+
 import scala.collection.mutable
-
-object Syntax {
-  type Role = String
-  type Name = String
-  type Agent = Int
-
-  sealed abstract class Exp
-  case class Var(role: Role, name: Name) extends Exp
-  case class BinOp(op: (Value, Value) => Value, left: Exp, right: Exp) extends Exp
-  case class UnOp(op: Value => Value, e: Exp) extends Exp
-
-  sealed abstract class Value extends Exp
-  case class Num(n: Int) extends Value
-  case class Bool(t: Boolean) extends Value
-
-  sealed abstract class Action
-  case class Private() extends Action
-  case class Publish(w: Exp) extends Action
-  case class Join(single: Boolean) extends Action
-
-  sealed abstract class Stmt
-  case class Assign(name: Name, e: Exp) extends Stmt
-
-  case class Fold(exp: Exp, v: Name, init: Value)
-
-  case class LocalStep(action: Action, name: Name)
-
-  case class BigStep(action: Map[Role, (LocalStep, Option[Fold])], timeout: Int, commands: Seq[Stmt])
-
-  case class Program(steps: Seq[BigStep])
-}
-
 import Syntax._
+import Op.Op
+import Op1.Op1
+
 
 object Model {
   type Scope = mutable.Map[Var, Value]
@@ -43,28 +15,9 @@ object Model {
 
   def time = 0
 
-  def require(condition: Boolean): Unit = { }
-
-  def eval(e: Exp, ctx: Scope): Value = {
-    e match {
-      case x @ Num(_) => x
-      case x @ Bool(_) => x
-      case v @ Var(_, _) => ctx(v)
-      case UnOp(op, arg) => op(eval(arg, ctx))
-      case BinOp(op, left, right) => op(eval(left, ctx), eval(right, ctx))
-    }
-  }
-
-  def hash(value: Value): Num = Num(value.hashCode)
-
-  def pre_step(s: BigStep): Unit = {
-    for ( (k, (step, Some(fold))) <- s.action)
-      global += (Var("Global", step.name) -> eval(fold.init, global))
-  }
-
   def receive_request(s: BigStep, sender: Agent, role: Role, value: Value): Unit = {
     // assume each sender must only send one message
-    val (LocalStep(action, name), maybeFold) = s.action(role)
+    val (LocalStep(action, name), fold) = s.action(role)
     val v = Var(role, name)
     val local = Γ(sender)
     require(!requests.get(sender).contains(v))
@@ -72,21 +25,23 @@ object Model {
       case Join(single) =>
         if (single) require(!owner.contains(role))
         owner.put(role, sender)
+
       case Private() =>
         require(owner.get(role).contains(sender))
+
       case Publish(where) =>
         require(owner.get(role).contains(sender))
         require(hash(value) == local(v))
         require(eval(where, global ++ local + (v -> value)) != Bool(false))
-        for (fold <- maybeFold) {
-          global += (Var("Global", fold.v) -> eval(fold.exp, global ++ local))
-        }
     }
+    // Fold is required; when not specified (for singleton roles),
+    // it is a simple assignment from local to global.
+    global += fold.get.v -> eval(fold.get.exp, global ++ local)
     requests.put(sender, v)
   }
 
   def progress(s: BigStep): Unit = {
-    val vars = (s.action map { case (role, (LocalStep(_, v), fold)) => Var(role, v) }).toSet
+    val vars = (s.action map { case (role, (LocalStep(_, v), _)) => Var(role, v) }).toSet
     val assigned = requests.values.toSet // TODO: perhaps testing fold result is better
     assert(assigned.subsetOf(vars))
     require(s.timeout <= time || vars == assigned)
@@ -97,4 +52,44 @@ object Model {
       // fire event here
     }
   }
+
+  def require(condition: Boolean): Unit = { }
+
+  def sem(op: Op1) (e: Value) : Value = {
+    (op, e)  match {
+      case (Op1.NOT, Bool(x)) =>  Bool(!x)
+      case (Op1.MINUS, Num(x)) => Num(-x)
+      case _ => throw Exception
+    }
+  }
+
+  def sem(op: Op) (left: Value, right: Value) : Value = {
+    (op, left, right)  match {
+      case (Op.EQ, _, _) =>  Bool(left == right)
+      case (Op.LT, Num(x), Num(y)) => Bool(x < y)
+      case (Op.ADD, Num(x), Num(y)) => Num(x + y)
+      case (Op.SUB, Num(x), Num(y)) => Num(x - y)
+      case (Op.MAX, Num(x), Num(y)) => Num(Math.max(x, y))
+      case _ => throw Exception
+    }
+  }
+
+  def eval(e: Exp, ctx: Scope): Value = {
+    e match {
+      case x @ Num(_) => x
+      case x @ Bool(_) => x
+      case v @ Var(_, _) => ctx(v)
+      case UnOp(op, arg) => sem(op)(eval(arg, ctx))
+      case BinOp(op, left, right) => sem(op)(eval(left, ctx), eval(right, ctx))
+    }
+  }
+
+  def hash(value: Value): Num = Num(value.hashCode)
+
+  def pre_step(s: BigStep): Unit = {
+    // FIX: Should happen at the global phase of previous step, to avoid the loop
+    for ( (_, (_, Some(fold))) <- s.action)
+      global += (fold.v -> eval(fold.init, global))
+  }
+
 }
---
+++ bsp/scala/src/main/bsp/Syntax.scala
@@ -0,0 +1,81 @@
+
+object Syntax {
+  type Role = String
+  type Name = String
+  type Agent = Int
+
+  object Op extends Enumeration {
+    type Op = Value
+    val EQ, LT, ADD, SUB, MAX = Value
+  }
+  import Op.Op
+
+  object Op1 extends Enumeration {
+    type Op1 = Value
+    val NOT, MINUS = Value
+  }
+  import Op1.Op1
+
+  sealed abstract class Exp
+  case class Var(role: Role, name: Name) extends Exp
+  case class BinOp(op: Op, left: Exp, right: Exp) extends Exp
+  case class IfThenElse(cond: Exp, left: Exp, right: Exp) extends Exp
+  case class UnOp(op: Op1, e: Exp) extends Exp
+
+  sealed abstract class Value extends Exp
+  case class Num(n: Int) extends Value
+  case class Bool(t: Boolean) extends Value
+
+  sealed abstract class Action
+  case class Private() extends Action
+  case class Publish(where: Exp = Bool(true)) extends Action
+  case class Join(single: Boolean = true) extends Action
+
+  sealed abstract class Stmt
+  case class Assign(name: Name, e: Exp) extends Stmt
+
+  case class Fold(exp: Exp, v: Var, init: Value)
+
+  case class LocalStep(action: Action, name: Name)
+
+  case class BigStep(action: Map[Role, (LocalStep, Option[Fold])], timeout: Int, commands: Seq[Stmt])
+
+  case class Program(steps: Seq[BigStep])
+}
+
+import Syntax._
+
+object Examples {
+  val OddsEvens = Program(Seq[BigStep](
+    BigStep(
+      action=Map(
+        "OddPlayer"  -> (LocalStep(Join(), "OddPlayer" ), None),
+        "EvenPlayer" -> (LocalStep(Join(), "EvenPlayer"), None)
+      ),
+      timeout = -1,
+      commands=Seq[Stmt]()
+    ),
+    BigStep(
+      action=Map(
+        "OddPlayer"  -> (LocalStep(Private(), "c"), None),
+        "EvenPlayer" -> (LocalStep(Private(), "c"), None)
+      ),
+      timeout=1,
+      commands=Seq[Stmt]()
+    ),
+    BigStep(
+      action=Map(
+        "OddPlayer"  -> (LocalStep(Publish(), "c"), None),
+        "EvenPlayer" -> (LocalStep(Publish(), "c"), None)
+      ),
+      timeout=1,
+      commands=Seq[Stmt](
+        Assign("Winner", IfThenElse(
+          BinOp(Op.EQ, Var("OddPlayer", "c"), Var("EvenPlayer", "c")),
+          Bool(true),
+          Bool(false))
+        )
+      )
+    )
+  ))
+}
